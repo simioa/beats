6a33fe4981d5990632dcf7e0493b8f4ae4af53ed
Ioannis Simeonidis <ioannis.simeonidis@dm.de>
2020-07-11 21:23:33 +0200

    add system.process_state metricset

---
+++ metricbeat/module/system/process_state/process_state_test.go
@@ -0,0 +1,79 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// +build darwin freebsd linux windows
+
+package process_state
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/elastic/beats/v7/libbeat/common"
+	mbtest "github.com/elastic/beats/v7/metricbeat/mb/testing"
+)
+
+func TestData(t *testing.T) {
+	f := mbtest.NewReportingMetricSetV2Error(t, getConfig())
+	err := mbtest.WriteEventsReporterV2Error(f, t, ".")
+	if err != nil {
+		t.Fatal("write", err)
+	}
+}
+
+func TestFetch(t *testing.T) {
+	f := mbtest.NewReportingMetricSetV2Error(t, getConfig())
+	events, errs := mbtest.ReportingFetchV2Error(f)
+
+	require.Empty(t, errs)
+	require.NotEmpty(t, events)
+	event := events[0].BeatEvent("system", "process_state").Fields
+	t.Logf("%s/%s event: %+v", f.Module().Name(), f.Name(),
+		event.StringToPrint())
+
+	summary, err := event.GetValue("system.process_state")
+	require.NoError(t, err)
+
+	event, ok := summary.(common.MapStr)
+	require.True(t, ok)
+
+	assert.Contains(t, event, "running")
+
+	eventProcess, ok := event["process"].(common.MapStr)
+	require.True(t, ok)
+
+	assert.Contains(t, eventProcess, "args")
+	assert.Contains(t, eventProcess, "alias")
+	assert.Contains(t, eventProcess, "name")
+
+}
+
+func getConfig() map[string]interface{} {
+	return map[string]interface{}{
+		"module":     "system",
+		"metricsets": []string{"process_state"},
+		"process_state.processes": []procMatcher{
+			{
+				Alias: "metricbeat",
+				Name:  "metricbeat",
+				Args:  ".*",
+			},
+		},
+	}
+}
